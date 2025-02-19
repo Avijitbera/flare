a3a9777e9b66dfea972e342e60b1828393cdeb20
Avijitbera <avijitbera775@gmail.com>
2025-02-19 22:45:01 +0530

    created response send function

--- lib/Response.dart
+++ lib/Response.dart
@@ -5,4 +5,14 @@ import 'dart:io';
 class Response {
   final Socket socket;
   Response(this.socket);
+
+  void send(String data){
+    final response = 'HTTP/1.1 200 OK\r\n'
+        'Content-Type: text/plain\r\n'
+        'Content-Length: ${data.length}\r\n'
+        '\r\n'
+        '$data'; 
+
+    socket.write(response);
+  }
 }
